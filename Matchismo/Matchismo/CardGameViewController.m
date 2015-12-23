62c17d19ece6052fef19ddeb9c76d08a4a6b9384
shiziwen <shiziwen@gmail.com>
2015-12-24 04:03:12 +0800

    codes for assignment 1 changed png added codes, when click the button, show random card

--- Matchismo/Matchismo/CardGameViewController.m
+++ Matchismo/Matchismo/CardGameViewController.m
@@ -7,17 +7,38 @@
 //
 
 #import "CardGameViewController.h"
+#import "Deck.h"
+#import "PlayingCardDeck.h"
 
 @interface CardGameViewController ()
+
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
 @property (nonatomic) int flipCount;
+
+@property (strong, nonatomic) Deck *deck;
 
 @end
 
 @implementation CardGameViewController
+
+// lazy instantiation
+- (Deck *)deck {
+    if (!_deck) {
+        _deck = [self createDeck];
+    }
+    
+    return _deck;
+}
+
+- (Deck *)createDeck {
+    return [[PlayingCardDeck alloc] init];
+}
+
 - (void) setFlipCount:(int)flipCount {
     _flipCount = flipCount;
     self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
+    
+    NSLog(@"flipCount changed to %d", flipCount);
 }
 
 // 点击事件
@@ -26,8 +47,11 @@
         [sender setBackgroundImage:[UIImage imageNamed:@"cardback"] forState:UIControlStateNormal];
         [sender setTitle:@"" forState:UIControlStateNormal];
     } else {
-        [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"] forState:UIControlStateNormal];
-        [sender setTitle:@"A♣︎" forState:UIControlStateNormal];
+        Card *randomCard = [self.deck drawRandomCard];
+        if (randomCard) {
+            [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"] forState:UIControlStateNormal];
+            [sender setTitle:randomCard.contents forState:UIControlStateNormal];
+        }
     }
     
     self.flipCount ++;
